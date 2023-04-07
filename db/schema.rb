ce48a0578f13c27ad0fb966a150dfebc896e8f73
Kohji-Saitoh <koji.s102030@gmail.com>
2023-04-07 11:39:17 +0900

    usersテーブルにnameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2023_04_06_024424) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
