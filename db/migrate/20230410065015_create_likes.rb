5bde6c56e5e1e87f6ef71c757f07b98875e9c37d
Kohji-Saitoh <koji.s102030@gmail.com>
2023-04-10 15:56:40 +0900

    likesテーブル作成

--- db/migrate/20230410065015_create_likes.rb
+++ db/migrate/20230410065015_create_likes.rb
@@ -1,6 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[6.0]
   def change
     create_table :likes do |t|
+      t.references :user, foreign_key: true         #追記
+      t.references :spot, foreign_key: true         #追記
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_10_051907) do
+ActiveRecord::Schema.define(version: 2023_04_10_065015) do
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "spot_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["spot_id"], name: "index_likes_on_spot_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -35,5 +44,7 @@ ActiveRecord::Schema.define(version: 2023_04_10_051907) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "spots"
+  add_foreign_key "likes", "users"
   add_foreign_key "spots", "users"
 end
