44aa9f42ce590688b786d6561db6dee01e7fd5cc
Kohji-Saitoh <koji.s102030@gmail.com>
2023-04-10 15:15:37 +0900

    spotsにバリデーション追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,9 @@
 class Spot < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
+  belongs_to :region
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :region_id, presence: true, numericality: {greater_than: 1}
 end
