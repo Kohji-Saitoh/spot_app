95f9ecab7b45efa755e1b37c887b0a00b4695f50
Kohji-Saitoh <koji.s102030@gmail.com>
2023-04-11 11:28:35 +0900

    各モデルにアソシエーションの追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,6 +3,8 @@ class Spot < ApplicationRecord
 
   belongs_to :user
   belongs_to :region
+  has_many :likes, dependent: :destroy
+  
   validates :name, presence: true
   validates :content, presence: true
   validates :region_id, presence: true, numericality: {greater_than: 1}
