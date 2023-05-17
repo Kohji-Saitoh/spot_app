6b747fb72e8057d3eaf7e637c866b30058c60ef8
Kohji-Saitoh <koji.s102030@gmail.com>
2023-05-17 11:53:11 +0900

    不要コメント削除

--- db/migrate/20230410065015_create_likes.rb
+++ db/migrate/20230410065015_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[6.0]
   def change
     create_table :likes do |t|
-      t.references :user, foreign_key: true         #追記
-      t.references :spot, foreign_key: true         #追記
+      t.references :user, foreign_key: true
+      t.references :spot, foreign_key: true
 
       t.timestamps
     end
